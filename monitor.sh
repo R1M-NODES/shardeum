16eb28cd8f3d9874efd732c377a037a7fd57321d
Sorkand <96412234+sorkand1@users.noreply.github.com>
2024-12-23 12:12:39 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,14 +3,21 @@
 while true; do
   #run
   date=$(date '+%Y/%m/%d %H:%M:%S')
-  status=$(docker exec -it shardeum-dashboard operator-cli status | awk '/state:/ {print $NF}')
+  container_name="shardeum-validator"
+  status=$(docker exec -it $container_name operator-cli status | awk '/state:/ {print $NF}' 2>/dev/null)
+  
+  if [[ -z "$status" ]]; then
+    echo "Error: Container $container_name not found or operator-cli is not accessible."
+    exit 1
+  fi
+  
   echo "Node status: $status"
   if [[ "$status" == *"top"* ]]; then
     echo "Your node stopped and I am trying to start now: $date"
     sleep 2
-    docker exec -it shardeum-dashboard operator-cli start
+    docker exec -it $container_name operator-cli start
     sleep 10
-    status=$(docker exec -it shardeum-dashboard operator-cli status | awk '/state:/ {print $NF}')
+    status=$(docker exec -it $container_name operator-cli status | awk '/state:/ {print $NF}')
     echo "Node status: $status"
   else
     sleep 1
